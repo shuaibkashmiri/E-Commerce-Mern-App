a627069a46b933656e22a2b325b620d07fbdc2dd
shuaibkashmiri <114304104+shuaibkashmiri@users.noreply.github.com>
2024-11-21 12:05:26 +0530

    deploy fixes

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ import connectDb from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import path from "path";
+
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+// Define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 const PORT = process.env.PORT || 8080;
@@ -20,6 +26,7 @@ connectDb();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+// Your Express setup
 app.use(express.static(path.join(__dirname, "./client/build")));
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -28,7 +35,7 @@ app.use("/api/v1/category", categoryRoutes);
 
 app.use("/api/v1/product", productRoutes);
 
-app.use("*", (req, res) => {
+app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
